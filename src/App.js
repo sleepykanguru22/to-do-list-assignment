0ac816ad7dfae7052d95e5af892829e845e6a77b
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-07-13 15:20:34 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,76 @@ import "./App.css";
 
 function App() {
   const [newToDo, setNewToDo] = useState("");
+  const [toDo, setToDo] = useState([]);
+
+  const handleNewToDoSubmit = (event) => {
+    event.preventDefault();
+
+    const toDo = {
+      text: newToDo,
+      complete: false
+    };
+
+    setToDo([...toDo, newToDo]);
+    setNewToDo("");
+  };
+
+  const handleToDoDelete = (deleteIdx) => {
+    const filteredTodo = toDo.filter((toDo, i) => {
+      return i !== deleteIdx;
+    });
+    setToDo(filteredTodo);
+  };
+
+  const handleCheckBox = (idx) => {
+    const updated = toDo.map((toDo, i) => {
+      if (idx === i) {
+        toDo.complete = !toDo.complete;
+      }
+      return toDo;
+    });
+    setToDo(updated);
+  };
+
   return (
     <div className="">
-      <header className="To Do List"></header>
+      <header className="">To Do List</header>
+      <form
+        onSubmit={(event) => {
+          handleNewToDoSubmit(event);
+        }}
+      >
+        <textarea
+          onChange={(event) => {
+            setNewToDo(event.target.value);
+          }}
+          value={newToDo}
+        ></textarea>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+      {toDo.map((toDo, i) => {
+        return (
+          <div key={i}>
+            <input
+              onChange={(event) => {
+                handleCheckBox(i);
+              }}
+              checked={toDo.complete}
+              type="checkbox"
+            />
+            <span>{toDo.text}</span>
+            <button
+              onClick={(event) => {
+                handleToDoDelete(i);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
